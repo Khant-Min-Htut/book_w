fd10116db59cabc3f16f4d6e992a0308998339af
khantMinHtut <khantminhtut009@gmail.com>
2025-10-30 14:58:59 +0700

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import job from "./lib/cron.js";
-import path from "path";
 
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __dirname = path.resolve();
 
 job.start();
 app.use(express.json());
@@ -19,15 +18,7 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../mobile/dist")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../mobile/dist", "index.html"));
-  });
-}
-
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
   connectDB();
 });
